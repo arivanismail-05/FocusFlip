587969a3ac08193e404bdf668a1ff2de3f8e2aab
Arivan Ismail <187494087+arivanismail-05@users.noreply.github.com>
2026-02-28 12:44:53 +0300

    Add Login, SignUp, and Splash activities
    - Create `SplashActivity` as the new entry point with navigation logic to Login or SignUp.
    - Implement `LoginActivity` and `SignUpActivity` with their respective layout files.
    - Update `AndroidManifest.xml` to register new activities and set `SplashActivity` as the launcher.

---
+++ app/src/main/java/com/example/focusflip/SplashActivity.java
@@ -0,0 +1,42 @@
+package com.example.focusflip;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+
+
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_splash);
+
+        if(resigter.getBoolean("isRegistered",false)){
+            Intent LoginActivity = new Intent(SplashActivity.this, LoginActivity.class);
+            startActivity(LoginActivity);
+            finish();
+        }
+
+        LinearLayout main = findViewById(R.id.main);
+
+        main.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent SignUpActivity = new Intent(SplashActivity.this, SignUpActivity.class);
+                startActivity(SignUpActivity);
+                finish();
+            }
+        });
+    }
+}
